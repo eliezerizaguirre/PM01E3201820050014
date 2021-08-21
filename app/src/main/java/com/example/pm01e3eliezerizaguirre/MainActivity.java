251c8f3c0770dfc8110d13e93c3a766eebde327d
eliezerizaguirre <eliezer65izaguirre@gmail.com>
2021-08-21 04:31:02 -0600

    P01E3201820050014

---
+++ app/src/main/java/com/example/pm01e3eliezerizaguirre/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.pm01e3eliezerizaguirre;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.pm01e3eliezerizaguirre.adaptadores.ListaMedicamentosAdapter;
+import com.example.pm01e3eliezerizaguirre.db.DbMedicamentos;
+import com.example.pm01e3eliezerizaguirre.entidades.Medicamentos;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView listaMedicamentos;
+    ArrayList<Medicamentos> listaArrayMedicamentos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listaMedicamentos = findViewById(R.id.listaMedicamentos);
+        listaMedicamentos.setLayoutManager(new LinearLayoutManager(this));
+
+        DbMedicamentos dbMedicamentos = new DbMedicamentos(MainActivity.this);
+
+        listaArrayMedicamentos = new ArrayList<>();
+
+        ListaMedicamentosAdapter adapter = new ListaMedicamentosAdapter(dbMedicamentos.mostrarMedicamentos());
+        listaMedicamentos.setAdapter(adapter);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menuNuevo:
+                nuevoRegistro();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void nuevoRegistro(){
+        Intent intent = new Intent(this, NuevoActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/pm01e3eliezerizaguirre/NuevoActivity.java
@@ -0,0 +1,159 @@
+package com.example.pm01e3eliezerizaguirre;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.pm01e3eliezerizaguirre.db.DbMedicamentos;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
+
+public class NuevoActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int PETICION_ACCESO_CAM = 100;
+
+    EditText txtDescripcion, txtCantidad, txtTiempo, txtPeriocidad;
+    //Spinner spinnerHD;
+    ImageView imgMedicamentos;
+    Button btnGuarda;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo);
+/*
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerHD);
+
+        ArrayList<String> elementos = new ArrayList<>();
+        elementos.add("");
+        elementos.add("Horas");
+        elementos.add("Diaria");
+
+        ArrayAdapter adp = new ArrayAdapter(NuevoActivity.this, android.R.layout.simple_spinner_dropdown_item, elementos);
+        spinner.setAdapter(adp);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                txtTiempo.getText();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
+*/
+        txtDescripcion = findViewById(R.id.txtDescripcion);
+        txtCantidad = findViewById(R.id.txtCantidad);
+        txtTiempo = findViewById(R.id.txtTiempo);
+        txtPeriocidad = findViewById(R.id.txtPeriocidad);
+        imgMedicamentos = findViewById(R.id.imgMedicamento);
+        btnGuarda = findViewById(R.id.btnGuarda);
+
+        btnGuarda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                Bitmap photo = ((BitmapDrawable)imgMedicamentos.getDrawable()).getBitmap();
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                photo.compress(Bitmap.CompressFormat.PNG, 100, bos);
+                byte[] bArray = bos.toByteArray();
+                 */
+               if(!txtDescripcion.getText().toString().equals("") && !txtCantidad.getText().toString().equals("")) {
+
+                    DbMedicamentos dbMedicamentos = new DbMedicamentos(NuevoActivity.this);
+                    long id = dbMedicamentos.insertarMedicamentos(txtDescripcion.getText().toString(), txtCantidad.getText().toString(), txtTiempo.getText().toString(), txtPeriocidad.getText().toString(), imgMedicamentos.getDrawable());
+
+                    if (id > 0) {
+                        Toast.makeText(NuevoActivity.this, "REGISTRO GUARDADO", Toast.LENGTH_LONG).show();
+                        limpiar();
+                    } else {
+                        Toast.makeText(NuevoActivity.this, "ERROR AL GUARDAR REGISTRO", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(NuevoActivity.this, "DEBE LLENAR LOS CAMPOS OBLIGATORIOS", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        imgMedicamentos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                permisos();
+            }
+        });
+
+    }
+    private void limpiar() {
+        txtDescripcion.setText("");
+        txtCantidad.setText("");
+        txtPeriocidad.setText("");
+        txtTiempo.setText("");
+    }
+
+    private void permisos() {
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PETICION_ACCESO_CAM);
+        }else{
+            tomarfoto();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == PETICION_ACCESO_CAM){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                tomarfoto();}
+        }else{
+            Toast.makeText(getApplicationContext(), "Se necesitan permisos de acceso", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void tomarfoto(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgMedicamentos.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public void clickFoto(View view) {
+        permisos();
+    }
+
+}
